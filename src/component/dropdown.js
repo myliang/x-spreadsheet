2418b367589fa64cf9448cabfe57310868bec40f
myliang <liangyuliang0335@126.com>
2019-03-27 22:36:18 +0800

    hide it when clickoutside dropdown

--- src/component/dropdown.js
+++ src/component/dropdown.js
@@ -43,7 +43,7 @@ export default class Dropdown extends Element {
     contentEl.show();
     this.parent().active();
     bindClickoutside(this.parent(), () => {
-      contentEl.hide();
+      this.hide();
     });
   }
 
