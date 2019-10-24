6f639699669d91cc27f6f9bcef89dff3263a6af0
Anand <a.sekar@alientt.com>
2019-10-24 13:55:22 +0530

    Minor fixes

--- src/core/cell.js
+++ src/core/cell.js
@@ -10,97 +10,100 @@ const infixExprToSuffixExpr = (src) => {
   let fnArgType = 0; // 1 => , 2 => :
   let fnArgOperator = '';
   let fnArgsLen = 1; // A1,A2,A3...
-  for (let i = 0; i < src.length; i += 1) {
-    const c = src.charAt(i);
-    if (c !== ' ') {
-      if (c >= 'a' && c <= 'z') {
-        subStrs.push(c.toUpperCase());
-      } else if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || c === '.') {
-        subStrs.push(c);
-      } else if (c === '"') {
-        i += 1;
-        while (src.charAt(i) !== '"') {
-          subStrs.push(src.charAt(i));
+  if(src !== undefined ){
+    for (let i = 0; i < src.length; i += 1) {
+      const c = src.charAt(i);
+      if (c !== ' ') {
+        if (c >= 'a' && c <= 'z') {
+          subStrs.push(c.toUpperCase());
+        } else if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || c === '.') {
+          subStrs.push(c);
+        } else if (c === '"') {
           i += 1;
-        }
-        stack.push(`"${subStrs.join('')}`);
-        subStrs = [];
-      } else {
-        // console.log('subStrs:', subStrs.join(''), stack);
-        if (c !== '(' && subStrs.length > 0) {
-          stack.push(subStrs.join(''));
-        }
-        if (c === ')') {
-          let c1 = operatorStack.pop();
-          if (fnArgType === 2) {
-            // fn argument range => A1:B5
-            try {
-              const [ex, ey] = expr2xy(stack.pop());
-              const [sx, sy] = expr2xy(stack.pop());
-              // console.log('::', sx, sy, ex, ey);
-              let rangelen = 0;
-              for (let x = sx; x <= ex; x += 1) {
-                for (let y = sy; y <= ey; y += 1) {
-                  stack.push(xy2expr(x, y));
-                  rangelen += 1;
-                }
-              }
-              stack.push([c1, rangelen]);
-            } catch (e) {
-              // console.log(e);
-            }
-          } else if (fnArgType === 1 || fnArgType === 3) {
-            if (fnArgType === 3) stack.push(fnArgOperator);
-            // fn argument => A1,A2,B5
-            stack.push([c1, fnArgsLen]);
-            fnArgsLen = 1;
-          } else {
-            // console.log('c1:', c1, fnArgType, stack, operatorStack);
-            while (c1 !== '(') {
-              stack.push(c1);
-              if (operatorStack.length <= 0) break;
-              c1 = operatorStack.pop();
-            }
-          }
-          fnArgType = 0;
-        } else if (c === '=' || c === '>' || c === '<') {
-          const nc = src.charAt(i + 1);
-          fnArgOperator = c;
-          if (nc === '=' || nc === '-') {
-            fnArgOperator += nc;
+          while (src.charAt(i) !== '"') {
+            subStrs.push(src.charAt(i));
             i += 1;
           }
-          fnArgType = 3;
-        } else if (c === ':') {
-          fnArgType = 2;
-        } else if (c === ',') {
-          if (fnArgType === 3) {
-            stack.push(fnArgOperator);
-          }
-          fnArgType = 1;
-          fnArgsLen += 1;
-        } else if (c === '(' && subStrs.length > 0) {
-          // function
-          operatorStack.push(subStrs.join(''));
+          stack.push(`"${subStrs.join('')}`);
+          subStrs = [];
         } else {
-          // priority: */ > +-
-          // console.log(operatorStack, c, stack);
-          if (operatorStack.length > 0 && (c === '+' || c === '-')) {
-            let top = operatorStack[operatorStack.length - 1];
-            if (top !== '(') stack.push(operatorStack.pop());
-            if (top === '*' || top === '/') {
-              while (operatorStack.length > 0) {
-                top = operatorStack[operatorStack.length - 1];
-                if (top !== '(') stack.push(operatorStack.pop());
-                else break;
+          // console.log('subStrs:', subStrs.join(''), stack);
+          if (c !== '(' && subStrs.length > 0) {
+            stack.push(subStrs.join(''));
+          }
+          if (c === ')') {
+            let c1 = operatorStack.pop();
+            if (fnArgType === 2) {
+              // fn argument range => A1:B5
+              try {
+                const [ex, ey] = expr2xy(stack.pop());
+                const [sx, sy] = expr2xy(stack.pop());
+                // console.log('::', sx, sy, ex, ey);
+                let rangelen = 0;
+                for (let x = sx; x <= ex; x += 1) {
+                  for (let y = sy; y <= ey; y += 1) {
+                    stack.push(xy2expr(x, y));
+                    rangelen += 1;
+                  }
+                }
+                stack.push([c1, rangelen]);
+              } catch (e) {
+                // console.log(e);
+              }
+            } else if (fnArgType === 1 || fnArgType === 3) {
+              if (fnArgType === 3) stack.push(fnArgOperator);
+              // fn argument => A1,A2,B5
+              stack.push([c1, fnArgsLen]);
+              fnArgsLen = 1;
+            } else {
+              // console.log('c1:', c1, fnArgType, stack, operatorStack);
+              while (c1 !== '(') {
+                stack.push(c1);
+                if (operatorStack.length <= 0) break;
+                c1 = operatorStack.pop();
               }
             }
+            fnArgType = 0;
+          } else if (c === '=' || c === '>' || c === '<') {
+            const nc = src.charAt(i + 1);
+            fnArgOperator = c;
+            if (nc === '=' || nc === '-') {
+              fnArgOperator += nc;
+              i += 1;
+            }
+            fnArgType = 3;
+          } else if (c === ':') {
+            fnArgType = 2;
+          } else if (c === ',') {
+            if (fnArgType === 3) {
+              stack.push(fnArgOperator);
+            }
+            fnArgType = 1;
+            fnArgsLen += 1;
+          } else if (c === '(' && subStrs.length > 0) {
+            // function
+            operatorStack.push(subStrs.join(''));
+          } else {
+            // priority: */ > +-
+            // console.log(operatorStack, c, stack);
+            if (operatorStack.length > 0 && (c === '+' || c === '-')) {
+              let top = operatorStack[operatorStack.length - 1];
+              if (top !== '(') stack.push(operatorStack.pop());
+              if (top === '*' || top === '/') {
+                while (operatorStack.length > 0) {
+                  top = operatorStack[operatorStack.length - 1];
+                  if (top !== '(') stack.push(operatorStack.pop());
+                  else break;
+                }
+              }
+            }
+            operatorStack.push(c);
           }
-          operatorStack.push(c);
+          subStrs = [];
         }
-        subStrs = [];
       }
     }
+
   }
   if (subStrs.length > 0) {
     stack.push(subStrs.join(''));
@@ -132,7 +135,8 @@ const evalSuffixExpr = (srcStack, formulaMap, cellRender, cellList) => {
   // console.log("srcstack",srcStack)
   for (let i = 0; i < srcStack.length; i += 1) {
     // console.log(':::>>>', srcStack[i]);
-    const expr = srcStack[i];
+    if(srcStack[i]!== undefined){
+      const expr = srcStack[i];
     const fc = expr[0];
     if (expr === '+') {
       const top = stack.pop();
@@ -174,6 +178,7 @@ const evalSuffixExpr = (srcStack, formulaMap, cellRender, cellList) => {
       stack.push(evalSubExpr(expr, cellRender));
     }
     // console.log('stack:', stack);
+    }
   }
   return stack[0];
 };
@@ -225,7 +230,7 @@ const cellRender = (src, formulaMap, getCellText, cellList = []) => {
     }else{
       stack = infixExprToSuffixExpr(src.substring(1))
     }
-    if (stack.length <= 0) return src;
+    if (stack.length && stack.length <= 0) return src;
     return evalSuffixExpr(
       stack,
       formulaMap,
