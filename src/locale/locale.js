df09bae03b955eeef127e5ff173b29ff33a5de59
myliang <liangyuliang0335@126.com>
2020-04-08 21:21:53 +0800

    x.spreadsheet => x_spreadsheet in locale.js

--- src/locale/locale.js
+++ src/locale/locale.js
@@ -23,8 +23,8 @@ function translate(key, messages) {
 
 function t(key) {
   let v = translate(key, $messages);
-  if (!v && window && window.x && window.x.spreadsheet && window.x.spreadsheet.$messages) {
-    v = translate(key, window.x.spreadsheet.$messages);
+  if (!v && window && window.x_spreadsheet && window.x_spreadsheet.$messages) {
+    v = translate(key, window.x_spreadsheet.$messages);
   }
   return v || '';
 }
