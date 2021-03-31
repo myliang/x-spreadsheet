c53bdb89423b7779eb5d5e1a66e20e1d2a58d205
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2021-03-31 16:33:22 -0400

    Add evaluation result to cell data

--- src/component/table.js
+++ src/component/table.js
@@ -78,10 +78,10 @@ export function renderCell(draw, data, rindex, cindex, yoffset = 0) {
     if(!data.settings.evalPaused) {
       const evalResult = _cell.render(cell.text || '', formulam, (y, x) => (data.getCellTextOrDefault(x, y)));
       cellText = evalResult;
-      if (cell.text !== evalResult){
+      if (cell.text !== evalResult && evalResult !== ""){
         cell.formulaValue = evalResult;
       }else{
-        if (cell.formulaValue) delete cell.formulaValue;
+        delete cell.formulaValue;
       }
     } else {
       cellText = cell.text || '';
