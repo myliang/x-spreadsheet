1c1437c1692e96dff8f242bc1ba2cc0cbf85dfbe
gafish <gafish@qq.com>
2021-05-11 12:57:59 +0800

    Fix return types for 'cellText' / 'loadData' / 'change'

--- src/index.d.ts
+++ src/index.d.ts
@@ -161,17 +161,17 @@ declare module 'x-data-spreadsheet' {
       colIndex: number,
       text: string,
       sheetIndex?: number
-    ): string;
+    ): this;
     /**
      * remove current sheet
      */
     deleteSheet(): void;
 
-    /**
+    /**s
      * load data
      * @param json
      */
-    loadData(json: Record<string, any>): void;
+    loadData(json: Record<string, any>): this;
     /**
      * get data
      */
@@ -180,13 +180,13 @@ declare module 'x-data-spreadsheet' {
      * bind handler to change event, including data change and user actions
      * @param callback
      */
-    change(callback: (json: Record<string, any>) => void): void;
+    change(callback: (json: Record<string, any>) => void): this;
     /**
      * set locale
      * @param lang
      * @param message
      */
-    locale(lang: string, message: string): void;
+    static locale(lang: string, message: object): void;
   }
   global {
     interface Window {
