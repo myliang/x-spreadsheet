974a7bc3d62a76aa2f2aee10bf0c22c8a47374f9
stefan.roggenstein <stefan.roggenstein@bergfreunde.de>
2021-12-29 14:06:22 +0100

    Remove log

--- src/component/table.js
+++ src/component/table.js
@@ -76,7 +76,6 @@ export function renderCell(draw, data, rindex, cindex, yoffset = 0) {
     // render text
     let cellText = '';
     if (!data.settings.evalPaused) {
-      console.log(cell.text);
       cellText = _cell.render(cell.text === 0 ? 0 : cell.text || '', formulam, (y, x) => (data.getCellTextOrDefault(x, y)));
     } else {
       cellText = cell.text === 0 ? 0 : cell.text || '';
