ee2a5f131305df23996c453ef0a91b5178f1e450
Alan Zhong <alan@ans.app>
2022-12-16 11:48:59 +0100

    Use autoFocus setting in other SelectorElements

--- src/component/selector.js
+++ src/component/selector.js
@@ -203,10 +203,10 @@ export default class Selector {
     const { autoFocus } = data.settings;
     this.inputChange = () => {};
     this.data = data;
-    this.br = new SelectorElement(true, autoFocus);
-    this.t = new SelectorElement();
-    this.l = new SelectorElement();
-    this.tl = new SelectorElement();
+    this.br = new SelectorElement(autoFocus, true);
+    this.t = new SelectorElement(autoFocus);
+    this.l = new SelectorElement(autoFocus);
+    this.tl = new SelectorElement(autoFocus);
     this.br.inputChange = (v) => {
       this.inputChange(v);
     };
