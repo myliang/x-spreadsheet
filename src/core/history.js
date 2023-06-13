0e7c207a19744f165efd4efd289689870c570bb7
Michael Strobel <michael.strobel@bergfreunde.de>
2023-06-13 08:43:19 +0200

    ENGARCH-371-fix-method-for-changed-cells

--- src/core/history.js
+++ src/core/history.js
@@ -1,4 +1,4 @@
-import helper from './helper';
+import helper from "./helper";
 
 export default class History {
   constructor() {
@@ -25,9 +25,7 @@ export default class History {
 
   add([data, selector]) {
     const [state] = this.undoItems.at(-1) || [{}];
-    this.undoItems.push(
-      [this.merge(data, state), selector],
-    );
+    this.undoItems.push([this.merge(data, state), selector]);
     this.redoItems = [];
   }
 
@@ -45,9 +43,7 @@ export default class History {
       const currentState = undoItems.pop();
       redoItems.push(currentState);
       const [state] = undoItems.at(-1) || [{}];
-      cb(
-        [this.merge(state), currentState[1]],
-      );
+      cb([this.merge(state), currentState[1]]);
     }
   }
 
@@ -56,42 +52,53 @@ export default class History {
     if (this.canRedo()) {
       const [state, selector] = redoItems.pop();
       undoItems.push([state, selector]);
-      cb(
-        [this.merge(state), selector],
-      );
+      cb([this.merge(state), selector]);
     }
   }
 
   getChangedCellValues() {
     const [state] = this.undoItems.at(-1) || [{}];
+
     const { rows } = state;
+
+    const parsedInitial = JSON.parse(this.initial);
+
     const getValue = (ri, ci) => {
       let val;
-      if (rows[ri].cells[ci]
-        && rows[ri].cells[ci].text
-          && ((this.initial[ri]
-            && this.initial[ri].cells[ci]
-              && (this.initial[ri].cells[ci].text !== rows[ri].cells[ci].text))
-              || !this.initial[ri]
-              || !this.initial[ri].cells[ci])) {
+
+      if (
+        rows[ri].cells[ci] &&
+        rows[ri].cells[ci].text &&
+        ((parsedInitial.rows[ri] &&
+          parsedInitial.rows[ri].cells[ci] &&
+          parsedInitial.rows[ri].cells[ci].text !== rows[ri].cells[ci].text) ||
+          !parsedInitial.rows[ri] ||
+          !parsedInitial.rows[ri].cells[ci])
+      ) {
         val = rows[ri].cells[ci].text;
       }
+
       return val;
     };
+
     if (rows) {
       const set = [];
+
       Object.keys(rows).forEach((ri) => {
         if (rows[ri].cells) {
           Object.keys(rows[ri].cells).forEach((ci) => {
             const value = getValue(ri, ci);
+
             if (value) {
               set.push({ ri: parseInt(ri, 10), ci: parseInt(ci, 10), value });
             }
           });
         }
       });
+
       return set;
     }
+
     return [];
   }
 
@@ -104,7 +111,8 @@ export default class History {
     const { undoItems } = this;
     if (undoItems.length === 0) return;
     const [item] = undoItems.at(stateIndex);
-    if (!item.rows[ri] || !item.rows[ri].cells || !item.rows[ri].cells[ci]) return;
+    if (!item.rows[ri] || !item.rows[ri].cells || !item.rows[ri].cells[ci])
+      return;
     if (item.rows[ri].cells[ci].text !== text) {
       item.rows[ri].cells[ci].text = text;
     }
